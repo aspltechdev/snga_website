7d6f871ee779841f0be37929ef795f3c5873433a
suriyaprakash23112002 <suriyaprakashkumaran567@gmail.com>
2026-09-12 14:14:01 +0530

    Fix GalleryPreview sport image

--- client/src/components/home/GalleryPreview.jsx
+++ client/src/components/home/GalleryPreview.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-icons/fa";
 import galleryService from "../../services/gallery.service";
 import "./GalleryPreview.css";
-import statementImage from "../../assets/sport.jpg";
+import statementImage from "../../assets/sport.JPG";
 
 const API_URL =
   import.meta.env.VITE_API_URL?.replace(/\/api\/?$/, "") ||
